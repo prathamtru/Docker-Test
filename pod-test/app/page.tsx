af5985955b4ec23d071e4c33e111cd050b43322e
Mukesh Kapoor <mukesh@truinc.com>
2024-11-25 14:40:12 +0530

    git push testing

--- pod-test/app/page.tsx
+++ pod-test/app/page.tsx
@@ -14,7 +14,7 @@ export default function UsersList() {
       try {
         const response = await fetch('/api/users');
         if (!response.ok) {
-          throw new Error('Failed to fetch users');
+          throw new Error('Failed to fetch users.');
         }
         const data = await response.json();
         setUsers(data); // Set the fetched users data into state
